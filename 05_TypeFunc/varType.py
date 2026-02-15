eb71b7747103b5cc7f51fe0f309107366b9f24df
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2026-02-15 20:17:53 +0530

    type function in python

---
+++ 05_TypeFunc/varType.py
@@ -0,0 +1,14 @@
+# integer type variable - int
+# num = 4345
+# print(num)
+# print(type(num))
+
+# float type variable - float
+# num = 34.565
+# print(num)
+# print(type(num))
+
+#string type variable - str
+name = "zeba"
+print(name)
+print(type(name))
